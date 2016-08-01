9266baeb3a80874a25c41c96f731270fe587848a
Chang <jenchang@gd-hcho-imac16.gdcb.iastate.edu>
2016-08-01 18:21:56 -0500

    add summary to tsv converter

---
+++ EbotScripts/convert2tsv.pl
@@ -0,0 +1,53 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $pmid="pmid";
+my $year="year";
+my $author="author";
+my $affiliation="affiliation";
+my $title="title";
+my $journal="journal";
+
+my $check="pmid";
+my $getyear=0;
+
+sub printoutput{
+    print("$pmid\t$year\t$journal\t$title\n");
+}
+
+printoutput;
+while(<>){
+    chomp;
+    if(/<\/PubmedArticle>/){
+	printoutput;
+	$pmid="pmid";
+    }
+    if(/<PMID Version="1">(\d+)<\/PMID>/){
+	if($pmid eq $check){
+	    $pmid=$1;
+	}
+    }
+    if(/<ISOAbbreviation>(.+)<\/ISOAbbreviation>/){
+	$journal=$1;
+    }
+    if(/<ArticleTitle>(.+)<\/ArticleTitle>/){
+	$title=$1;
+    }
+    if(/<ArticleDate /){
+	$getyear=1;
+    }
+    if(/<\/ArticleDate>/){
+	$getyear=0;
+    }
+    if(/<Year>(\d+)<\/Year>/){
+	if($getyear==1){
+	    $year=$1;
+	}
+    }
+    # check if monitor turns off, 10 min
+    # if not off turn off when I leave
+    
+}
+
