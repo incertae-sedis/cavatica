d8ecdd36d049ab1dbba949d6ed64f87a61fcf653
Jennifer Chang <jenchang@Jennifers-MacBook-Pro.local>
2016-08-20 22:26:48 -0500

    update array

--- EbotScripts/RUNALL.sh
+++ EbotScripts/RUNALL.sh
@@ -1,9 +1,12 @@
 #! /usr/bin/env bash
+set -u
+set -e
 
-declare -a arr=("igraph" "graphlab" "neo4j" "graphviz" "gephi" "visant" "cytoscape" "pathway+studio" "IPA+Ingenuity+Pathway+Analysis")
-mkdir data
+[[ -d data ]] || mkdir data
 
-for TERM in "${arr[@]}"
+arr="igraph graphlab neo4j graphviz gephi visant cytoscape pathway+studio IPA+Ingenuity+Pathway+Analysis"
+
+for TERM in $arr
 do	    
     echo $TERM "loading... PMC search"
 
@@ -15,14 +18,11 @@ do
     echo $TERM "loading... Pubmed search"
     perl searchpubmed.pl $TERM data/$TERM-pmidsD.txt
 
-    echo $TERM "merge ids and fetch final \n"
+    echo $TERM "merge ids and fetch final"
     cat data/$TERM-pmidsD.txt data/$TERM-pmids.txt | sort | uniq > data/$TERM-pmidsA.txt
     perl fetchPubmedData.pl data/$TERM-pmidsA.txt data/$TERM-pubmedA.xml
 
-    echo $TERM "generate final network files \n"
+    echo $TERM "generate final network files"
     perl convert2tsv.pl data/$TERM-pubmedA.xml > data/$TERM-papers.tsv
     perl getPM2Author.pl data/$TERM-pubmedA.xml > data/$TERM-coauthor.tsv
 done
-
-
-
