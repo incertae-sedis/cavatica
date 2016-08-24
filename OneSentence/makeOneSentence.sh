b7faa97d6d6e39ea930c8fe1e9b1271c2c29ed88
Jennifer Chang <jenchang@jenchang.student.iastate.edu>
2016-08-24 16:12:54 -0500

    loopify

--- OneSentence/makeOneSentence.sh
+++ OneSentence/makeOneSentence.sh
@@ -1,15 +1,17 @@
 #! /usr/bin/env bash
 
 INDIR="data20Years"
-arr="igraph neo4j gephi cytoscape pathway+studio IPA+Ingenuity+Pathway+Analysis"
+
+arr=`ls data20Years/* | tr '/' ' ' | sed 's/[-]/ /g' |awk '{print $2}'|uniq`
 
 for TERM in $arr
 do
-    perl pubmed_text_analyzer2.pl $TERM $INDIR/$TERM-pubmed.xml > $TERM.html
+    small=`ls data20Years/${TERM}-pubmed.xml | tr '/' ' ' | sed 's/-/ /g'| awk '{print $2}'| sed 's/+/ /g'`
+    perl pubmed_text_analyzer2.pl "$small" $INDIR/$TERM-pubmed.xml > $TERM.html
 done
 
 #Special cases
 perl pubmed_text_analyzer2.pl IPA $INDIR/IPA+Ingenuity+Pathway+Analysis-pubmed.xml > IPA.html
 perl pubmed_text_analyzer2.pl "Ingenuity Pathway Analysis" $INDIR/IPA+Ingenuity+Pathway+Analysis-pubmed.xml > Ingenuity+Pathway+Analysis.html
-perl pubmed_text_analyzer2.pl "pathway studio" $INDIR/pathway+studio-pubmed.xml > pathway+studio.html
+
 
