fa86d9062b38a785b3f0d1417873439324291b83
Chang <jenchang@gd-hcho-imac16.gdcb.iastate.edu>
2016-08-24 11:16:40 -0500

    R trips over strange names so only pull pubmed id and year

---
+++ MkFigures/prepareInput.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+set -u
+set -e
+
+OUTDIR=data
+[[ -d $OUTDIR ]] || mkdir $OUTDIR
+
+arr="neo4j gephi igraph cytoscape pathway+studio IPA+Ingenuity+Pathway+Analysis"
+
+for TERM in $arr
+do
+    awk '{print $1,$2}' data20Years/$TERM-papers.tsv > $OUTDIR/$TERM-years.ssv
+done
