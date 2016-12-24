98547b54dfa0499607f3b247a20865f3e09ff1a4
Jennifer Chang <jenchang@iastate.edu>
2016-12-24 12:37:09 -0600

    add citation info

--- 01-GetData/README.md
+++ 01-GetData/README.md
@@ -1,8 +1,15 @@
 Get Data
 --
 
-Use RISmed R package. All query terms and date ranges should be listed in the above ../config.txt file. You should be able to fetch the entire co-author network for each one of the networks using the following command. A bar chart (be default from 1996 to 2016) is automatically generated and saved to this folder.
+Use RISmed R package. All query terms and date ranges should be listed in the above ../config.txt file. You should be able to fetch the entire co-author network for each one of the networks using the following command. A bar chart (by default from 1996 to 2016) is automatically generated and saved to this folder.
 
+From bash command line:
 ```
 $ Rscript fetch.R
+```
+
+Citation for the RISmed R Package
+```
+  Stephanie Kovalchik (2016). RISmed: Download Content from NCBI Databases. R package version 2.1.6.
+  https://CRAN.R-project.org/package=RISmed
 ```
--- 01-GetData/fetch.R
+++ 01-GetData/fetch.R
@@ -1,21 +1,22 @@
-#install.packages("RISmed")
-#install.packages("ggplot2")
+# pkgs <- c("RISmed","ggplot2")
+#install.packages(pkgs)
 library(RISmed)
 library(ggplot2)
 dimwidth=8
 dimheight=4.916
-
 # =================================== Start Analysis
-terms <- read.table("../config.txt",sep=" ",stringsAsFactors=FALSE,header=FALSE)
-terms <- c(terms$V1)
-query_term <- terms[1]
-res <- EUtilsSummary(query_term,type='esearch',db='pubmed',datetype='pdat',mindate=1996,maxdate=2016,retmax=10000)
+terms <- read.table("../config.txt",sep=",",stringsAsFactors=FALSE,header=TRUE)
+i=8
+query_term <- terms$term[i]
+start <-terms$start[i]
+end <- terms$end[i]
+res <- EUtilsSummary(query_term,type='esearch',db='pubmed',datetype='pdat',mindate=start,maxdate=end,retmax=10000)
 #QueryCount(res)
 
 pmids <- EUtilsGet(res)
 
-data <- data.frame(year=YearPubmed(EUtilsGet(res)),title=ArticleTitle(EUtilsGet(res)))
 data <- data.frame(year=YearPubmed(pmids),title=ArticleTitle(pmids))
+
 
 p<-qplot(data$year,fill=I('royalblue'),binwidth=0.5,xlab="Year of Publication",ylab="Number of papers",
          main=paste("PubMed Articles containing '",query_term,"'; n=",length(data$year),sep=""))+
@@ -24,7 +25,7 @@ p<-qplot(data$year,fill=I('royalblue'),binwidth=0.5,xlab="Year of Publication",y
   geom_text(stat='count',aes(label=..count..),vjust=-0.25)+
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-if(ggplot_build(p)$panel$ranges[[1]]$y.range[2]<10){
+if(ggplot_build(p)$layout$panel_ranges[[1]]$y.range[2]<10){
     p<-qplot(data$year,fill=I('royalblue'),binwidth=0.5,xlab="Year of Publication",ylab="Number of papers",
            main=paste("PubMed Articles containing '",query_term,"'; n=",length(data$year),sep=""))+
     scale_x_continuous(breaks=seq(1996,2016,1),limits=c(1995.5,2016.5))+
@@ -36,5 +37,9 @@ if(ggplot_build(p)$panel$ranges[[1]]$y.range[2]<10){
 
 ggsave(filename=paste(query_term,"-pubmedcounts.png",sep=""),plot=p,width=dimwidth,height=dimheight,dpi=600)
 
-p
+# Get data
+# slotNames(pmids) # list all names
+data <- data.frame(pmid=PMID(pmids),year=YearPubmed(pmids),title=ArticleTitle(pmids),journal=Title(pmids),affiliation=Affiliation(pmids))
+write.table(data,file=paste(query_term,"-papers.tsv",sep=""),sep="\t",row.names=FALSE,col.names=TRUE,quote=FALSE)
 
+head(MedlineTA(pmids),n=100)
