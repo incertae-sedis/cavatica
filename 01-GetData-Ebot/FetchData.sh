25d20401569b1eb35996720f160de585ac480fcd
Chang <jenchang@iastate.edu>
2016-12-27 18:03:25 -0600

    ignore temp files

--- 01-GetData-Ebot/FetchData.sh
+++ 01-GetData-Ebot/FetchData.sh
@@ -14,7 +14,7 @@ do
     wc -l $OUTDIR/$TERM-pmids.txt
     perl fetchPubmedData.pl $OUTDIR/$TERM-pmids.txt $OUTDIR/$TERM-pubmed.xml
     perl convert2tsv.pl $OUTDIR/$TERM-pubmed.xml > $OUTDIR/$TERM-papers.tsv
-    perl getPM2Author.pl $OUTDIR/$TERM-pubmed.xml > $OUTDIR/$TERM-coauthors.tsv
+    perl getPM2Author.pl $OUTDIR/$TERM-pubmed.xml > $OUTDIR/$TERM-authors.tsv
 
     echo "Searching Pubmed Central between 1996 and 2016: $TERM"
     perl searchPMCBtwnPubDates.pl $TERM $OUTDIR/$TERM-pmcids.txt
