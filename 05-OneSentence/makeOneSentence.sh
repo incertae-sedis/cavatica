79e3e322d67ec362077eda74f8c131651218c740
Chang <jenchang@iastate.edu>
2017-01-04 12:44:11 -0600

    small edits

--- 05-OneSentence/makeOneSentence.sh
+++ 05-OneSentence/makeOneSentence.sh
@@ -27,15 +27,15 @@ TERM="VisANT"
 echo "Processing $TERM"
 query="VisA"
 perl pubmed_text_analyzer3.pl "$query" $INDIR/$TERM-pubmed.xml > $TERM.html
-perl pubmed_fulltext_analyzer_v4_C.pl "$query" $INDIR/$TERM-fulltext.xml > $TERM-full.html
-uniq pmcid.txt ${TERM}-pmcid.txt
+#perl pubmed_fulltext_analyzer_v4_C.pl "$query" $INDIR/$TERM-fulltext.xml > $TERM-full.html
 uniq pmid.txt ${TERM}-pmid.txt
 wc -l ${TERM}-pmid.txt
-wc -l $INDIR/${TERM}-pmids.tx
-wc -l ${TERM}-pmcid.txt
-wc -l $INDIR/${TERM}-pmcids.txt
+wc -l $INDIR/${TERM}-pmids.txt
+#uniq pmcid.txt ${TERM}-pmcid.txt
+#wc -l ${TERM}-pmcid.txt
+#wc -l $INDIR/${TERM}-pmcids.txt
 echo " ${TERM}.html saved"
-echo " ${TERM}-full.html saved"
+#echo " ${TERM}-full.html saved"
 
 
 
