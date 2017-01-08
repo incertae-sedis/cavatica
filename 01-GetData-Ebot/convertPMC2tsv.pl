c87f78b1df9646879cc4132427146788b026a5f5
Jennifer Chang <jenchang@iastate.edu>
2017-01-08 10:05:44 -0600

    CHOU CODE
    This is version four. Generated networks for Ingenuity
    not loading into R and Mango properly. Debug

--- 01-GetData-Ebot/convertPMC2tsv.pl
+++ 01-GetData-Ebot/convertPMC2tsv.pl
@@ -9,22 +9,22 @@ my $keyword = shift @ARGV;
 my $paper_file_name = shift @ARGV;
 my $author_file_name = shift @ARGV;
 
-my ($pmcid, $title, $year, $text);
+my ($pmid, $pmcid, $title, $year, $text, $shared_aff);
 my @authors;
 my %affiliations;
-my ($article_c, $pmcid_c, $title_c, $year_c, $text_c) = ( 0, 0, 0, 0, 0 );
+my ($article_c, $pmid_c, $pmcid_c, $title_c, $year_c, $text_c) = ( 0, 0, 0, 0, 0, 0 );
 
 $keyword = lc $keyword; # comment this line to do case sensitive search
 
 open PAPER, ">$paper_file_name" or die "can't open paper TSV file $paper_file_name to write";
 open AUTHOR, ">$author_file_name" or die "can't open author TSV file $author_file_name to write";
 
-print PAPER "pmid\tyear\tcount\ttitle\n";
+print PAPER "pmid\tyear\tcount\ttitle\totherid\n";
 print AUTHOR "pmid\tforename_lastname\taffiliation\n";
 
 while (<>) {
   if (/<article /) {
-    $pmcid = $title = $year = $text = "";
+    $pmid = $pmcid = $title = $year = $text = $shared_aff = "";
     @authors = ( );
     %affiliations = ( );
     $article_c++;
@@ -32,6 +32,9 @@ while (<>) {
     $pmcid = $1;
     #print STDERR "$pmcid\n";
     $pmcid_c++;
+  } elsif (/<article-id pub-id-type=\"pmid\">(\d+)<\/article-id>/ && length($pmid)==0) {
+    $pmid = $1;
+    $pmid_c++;
   } elsif (/<article-title>/ && length($title)==0) {
     chomp;
     $title = $_;
@@ -43,12 +46,13 @@ while (<>) {
     $title =~ /<article-title>(.+)<\/article-title>/;
     $title = $1;
     $title_c++;
-  } elsif (/<contrib contrib-type=\"author\">/) {
+  } elsif (/<contrib .*contrib-type=\"author\"/) {
     my @a;
     while ($_ !~ /<\/contrib>/) {
-      $a[0] = $1 if /<surname>(.+?)<\/surname>/;
-      $a[1] = $1 if /<given-names>(.+?)<\/given-names>/;
-      $a[2] = $1 if /<xref ref-type=\"aff\" rid=\"(.+?)\">/;
+      $a[1] = $1 if /<surname>(.+?)<\/surname>/;
+      $a[0] = $1 if /<given-names>(.+?)<\/given-names>/;
+      $a[2] = $1 if /<xref ref-type=\"aff\" rid=\"(\S+?)(\s|\")/ && !defined($a[2]);
+      $a[2] = $1 if /<xref rid=\"(\S+?)(\s|\") ref-type=\"aff\"/ && !defined($a[2]);
       $_ = <>;
     }
     unless (defined($a[0]) || defined($a[1])) {
@@ -56,11 +60,22 @@ while (<>) {
     } else {
       push @authors, \@a;
     }
-  } elsif (/<aff id=\"(.+?)\">(.+?)<\/aff>/) {
-    my ($i, $a) = ( $1, $2);
-    $a =~ s/^<([^>]+)>.+?<\/\1>//;
-    #$a =~ s/<.+?>//g; # uncomment this line if you want HTML markups removed, too
+  } elsif (/<aff id=\".+?\"/) {
+    chomp;
+    my ($i, $a) = ( "", $_);
+    while ($_ !~ /<\/aff>/) {
+      $_ = <>;
+      chomp;
+      $a .= $_;
+    }
+    $a =~ /<aff id=\"(.+?)\".*?>(.+)<\/aff>/ or die "unexpected pattern match errors";
+    ($i, $a) = ( $1, $2);
+    $a =~ s/^\s*(<([^>]+)>.{0,10}<\/\2>|<[^>]+\/>)\s*//;
+    $a =~ s/<email>.*<\/email>//g; # uncomment this line if you want email addresses removed
+    $a =~ s/<.+?>//g; # uncomment this line if you want HTML markups removed, too
     $affiliations{$i} = $a;
+  } elsif (/<aff>(.+?)<\/aff>/) {
+    $shared_aff = $1;
   } elsif (/<pub-date .*?(pub-type=\"[pe]pub\"|date-type=\"pub\")/ && length($year)==0) {
     while (1) {
       if (/<year.*?>(\d+)<\/year/) {
@@ -83,8 +98,9 @@ while (<>) {
     $count -= () = $copy =~ /\.$keyword\./g; # uncount cases like www.cytoscape.org
     if ($count>0) {
       if ($pmcid) {
-	print PAPER "$pmcid\t$year\t$count\t$title\n";
+	print PAPER "$pmcid\t$year\t$count\t$title\t$pmid\n";
 	warn "PMC id $pmcid has no year info" unless length($year);
+	warn "PMC id $pmcid has no author info" unless @authors>0;
 	for (@authors) {
 	  print AUTHOR "$pmcid\t";
 	  if (defined $_->[0]) {
@@ -97,8 +113,16 @@ while (<>) {
 	  } else {
 	    print AUTHOR "UNKNOWN\t";
 	  }
-	  if (defined($_->[2]) && defined($affiliations{$_->[2]})) {
-	    print AUTHOR "$affiliations{$_->[2]}\n";
+	  if (defined($_->[2])) {
+	    if (defined($affiliations{$_->[2]})) {
+	      print AUTHOR "$affiliations{$_->[2]}\n";
+	    } else {
+	      warn "missing affiliation info $_->[2] mentioned on author list for PMC $pmcid";
+	    }
+	  } elsif ($shared_aff) {
+	    print AUTHOR "$shared_aff\n";
+	  } elsif (keys(%affiliations)==1) {
+	    print AUTHOR values(%affiliations), "\n";
 	  } else {
 	    print AUTHOR "UNKNOWN\n";
 	  }
