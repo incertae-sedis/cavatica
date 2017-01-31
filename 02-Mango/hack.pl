bdaa4eb985410637d74b35c7a13d865e376bdb3b
Jennifer Chang <jenchang@iastate.edu>
2017-01-31 17:33:26 -0600

    works but ugly

--- 02-Mango/hack.pl
+++ 02-Mango/hack.pl
@@ -3,6 +3,8 @@
 use strict;
 use warnings;
 
+my @tools=("C","J","P","V","G","N","I","L","Z");
+
 # filename = temp-big.tsv
 my $count;
 my $left;
@@ -14,6 +16,8 @@ my $c="G";
 my %lo;  # left only
 my %ro;  # right only
 my %bo;  # both
+
+my %trans=();
 while(<>){
     chomp;
     if(/(\d+) (\S+):(\S+)/){
@@ -24,10 +28,43 @@ while(<>){
         $count=$1;
 	$left=$2;
 	$right=$3;
+
+	foreach $c (@tools){
+	    $cl=()=$left=~/$c/g;
+	    $cr=()=$right=~/$c/g;
+
+	    if($cl && $cl==$cr){
+		$bo{$c}++;
+	    }
+	    if($cl<$cr){
+		$ro{$c}++;
+	    }
+	    if($cr<$cl){
+		$lo{$c}++;
+	    }
+	    #print "$count $left|$right $cl $cr $c\n";
+	}
 	
-	$cl=()=$left=~/$c/g;
-	$cr=()=$right=~/$c/g;
-	
-	print "$count $left|$right $cl $cr\n";
+	foreach my $key (keys %lo){
+	    foreach my $r (keys %ro){
+		#print "$count $left:$right $key:$r LO\n";
+		$trans{"$key:$r"}+=$count;
+	    }
+	    foreach my $r (keys %bo){
+		#print "$count $left:$right $key:$r LO\n";
+		$trans{"$key:$r"}+=$count;
+	    }
+	}
+	foreach my $key (keys %ro){
+	    foreach my $r (keys %bo){
+		#print "$count $left:$right $r:$key RO\n";
+		$trans{"$r:$key"}+=$count;
+	    }
+	}
     }
 }
+
+foreach my $tr (sort keys %trans){
+    print "$tr $trans{$tr}\n";
+}
+
