1d5b942ae62ee9f2356ea6b3c1b7d535764d541d
Jennifer Chang <jenchang@iastate.edu>
2017-02-08 01:15:18 -0600

    Live
    Uncommented the fetch part of the script. Was using the convert to xml to tsv
    at the end of the script to avoid querying PubMed too many times.

--- 01-GetData-Ebot/FetchData.sh
+++ 01-GetData-Ebot/FetchData.sh
@@ -10,16 +10,16 @@ arr=`awk -F',' 'NR>1 {print $1}' ../config.txt`
 for TERM in $arr
 do	    
     echo "Searching Pubmed between 1996 and 2016: $TERM"
-#    perl searchPubmedBtwnPubDates.pl $TERM $OUTDIR/$TERM-pmids.txt
-#    wc -l $OUTDIR/$TERM-pmids.txt
-#    perl warpAroundfetchPubmedData.pl $OUTDIR/$TERM-pmids.txt $OUTDIR/$TERM-pubmed.xml
-#    perl convert2tsv.pl $OUTDIR/$TERM-pubmed.xml > $OUTDIR/$TERM-papers.tsv
-#    perl getPM2Author.pl $OUTDIR/$TERM-pubmed.xml > $OUTDIR/$TERM-authors.tsv
-#
-#    echo "Searching Pubmed Central between 1996 and 2016: $TERM"
-#    perl searchPMCBtwnPubDates.pl $TERM $OUTDIR/$TERM-pmcids.txt
-#    wc -l $OUTDIR/$TERM-pmcids.txt
-    #    perl warpAroundfetchPMCfulltext.pl $OUTDIR/$TERM-pmcids.txt $OUTDIR/$TERM-fulltext.xml
+    perl searchPubmedBtwnPubDates.pl $TERM $OUTDIR/$TERM-pmids.txt
+    wc -l $OUTDIR/$TERM-pmids.txt
+    perl warpAroundfetchPubmedData.pl $OUTDIR/$TERM-pmids.txt $OUTDIR/$TERM-pubmed.xml
+    perl convert2tsv.pl $OUTDIR/$TERM-pubmed.xml > $OUTDIR/$TERM-papers.tsv
+    perl getPM2Author.pl $OUTDIR/$TERM-pubmed.xml > $OUTDIR/$TERM-authors.tsv
+
+    echo "Searching Pubmed Central between 1996 and 2016: $TERM"
+    perl searchPMCBtwnPubDates.pl $TERM $OUTDIR/$TERM-pmcids.txt
+    wc -l $OUTDIR/$TERM-pmcids.txt
+    perl warpAroundfetchPMCfulltext.pl $OUTDIR/$TERM-pmcids.txt $OUTDIR/$TERM-fulltext.xml
     KEYWORD=`echo $TERM | sed 's/+/ /g'`
     perl convertPMC2tsv.pl "$KEYWORD" $OUTDIR/$TERM-pmc-papers.tsv $OUTDIR/$TERM-pmc-authors.tsv $OUTDIR/$TERM-fulltext.xml
 done
