3f74956d38efce943255e89b711acfa72fa6117d
Jennifer Chang <jenchang@iastate.edu>
2017-02-08 00:48:38 -0600

    Transition Matrix - Adoption Matrix
    Process the multi tool subnetwork file and count the transitions.
    The tool one letter codes are hard coded into hack.pl. Should be more general purpose.
    Fix later.

--- 02-Mango/counttrends.sh
+++ 02-Mango/counttrends.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
 
-sed -n '/#From/,$p' mult-pmc.tsv | awk -F'\t' '{print $2,"\t",$8}' |sort -k2,1 > trends-pmc.txt
+sed -n '/#From/,$p' $1 | awk -F'\t' '{print $2,"\t",$8}' |sort -k2,1 > trends-temp.txt
+perl printTransitions.pl trends-temp.txt > trends-temp2.txt
+awk -F'\t' '{print $4}' trends-temp2.txt |sort | uniq -c > trends-temp3.txt
+perl hack.pl trends-temp3.txt 
