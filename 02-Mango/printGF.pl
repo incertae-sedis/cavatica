7926e1dbeae70f569d16a7b63964e059cd68b7c1
Jennifer Chang <jenchang@iastate.edu>
2017-02-13 11:01:14 -0600

    Print Growth Factor from community files.
    GF += Diff counts / Diff Years for each cluster.
    
    ```
    perl printGF.pl GraphLab-pmc-by-community.tsv |grep "Total" |sort -nr |less
    ```

---
+++ 02-Mango/printGF.pl
@@ -0,0 +1,66 @@
+#! /usr/bin/env perl
+
+use strict;
+use warnings;
+
+my $com=-1;
+my $year=0;
+my $count=0;
+my $lcount=0;
+
+my $sum=0.0;
+my $dyear=0;
+
+my $papers=0;
+# when to print
+#  when new year except first year in community
+#  when new community
+#  end at last community
+
+sub printSum(){
+    print "  Sum: $count $lcount $dyear =",($count-$lcount)/$dyear,"\n";
+    $sum+=($count-$lcount)/$dyear;
+    $papers+=$count;
+}
+
+sub printTotal(){
+    print "$papers\t$com\tTotal=$sum\n";
+}
+
+while(<>){
+    chomp;
+    if(/^(\d+)\t(\d\d\d\d)\t/){
+
+	
+	if($com != $1){
+	    if($com>=0){
+		printSum();
+		printTotal();
+	    }
+	    $com=$1;
+	    $dyear=1;
+	    $year=$2;
+	    $lcount=0;
+	    $count=1;
+	    $sum=0.0;
+	    $papers=0;
+	    
+	}elsif($year!=$2){
+	    printSum();
+	    $lcount=$count;
+	    $count=1;
+	    $dyear=$2-$year;
+	    $year=$2;
+	}else{
+	    $count++;
+	}
+	
+	print "$1\t$2\n";
+    }
+    
+}
+
+if($com>=0){
+    printSum();
+    printTotal();
+}
