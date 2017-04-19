d05690c8738aba7e1991b3c4d78d0049c1023218
Chang <jenchang@iastate.edu>
2017-04-19 15:15:21 -0500

    Term from arguments
    * makeOneSentence.sh generates html summaries for one term, given as argument. If no arguments, then generates html summaries for all terms in config.txt
    * comment out the VisANT specific processing. Left in script file as an example to users on how to adjust code
    
    [] save a list of false positive ids. Can be used when re-fetching data at a later date
    [] Change the html so while user is scrolling, they can check a box to indicate id removal. Then at bottom of page, add a button to generate a false positive id file. Allows human curation of results.

--- 05-OneSentence/makeOneSentence.sh
+++ 05-OneSentence/makeOneSentence.sh
@@ -4,7 +4,15 @@ INDIR="../01-GetData-Ebot/DATAHERE"
 [[ -d $INDIR ]] || echo "No input files. Run 01-GetData-Ebot scripts first"
 [[ -d $INDIR ]] || exit 0
 
-arr=`awk -F',' 'NR>1 {print $1}' ../config.txt`
+arr=""
+
+# User can pass in a term, instead of using config.txt
+if [ $# -ge 1 ]
+then
+    arr=$1
+else
+    arr=`awk -F',' 'NR>1 {print $1}' ../config.txt`
+fi
 
 for TERM in $arr
 do
@@ -24,18 +32,18 @@ do
 done
 
 # specific for VisANT
-TERM="VisANT"
-echo "Processing $TERM"
-query="VisA"
-perl pubmed_text_analyzer3.pl "$query" $INDIR/$TERM-pubmed.xml > $TERM.html
-uniq pmid.txt ${TERM}-pmid.txt
-wc -l ${TERM}-pmid.txt
-wc -l $INDIR/${TERM}-pmids.txt
-perl pubmed_fulltext_analyzer_v4_C.pl "$query" $INDIR/$TERM-fulltext.xml > $TERM-full.html
-uniq pmcid.txt ${TERM}-pmcid.txt
-wc -l ${TERM}-pmcid.txt
-wc -l $INDIR/${TERM}-pmcids.txt
-echo " ${TERM}.html saved"
+#TERM="VisANT"
+#echo "Processing $TERM"
+#query="VisA"
+#perl pubmed_text_analyzer3.pl "$query" $INDIR/$TERM-pubmed.xml > $TERM.html
+#uniq pmid.txt ${TERM}-pmid.txt
+#wc -l ${TERM}-pmid.txt
+#wc -l $INDIR/${TERM}-pmids.txt
+#perl pubmed_fulltext_analyzer_v4_C.pl "$query" $INDIR/$TERM-fulltext.xml > $TERM-full.html
+#uniq pmcid.txt ${TERM}-pmcid.txt
+#wc -l ${TERM}-pmcid.txt
+#wc -l $INDIR/${TERM}-pmcids.txt
+#echo " ${TERM}.html saved"
 #echo " ${TERM}-full.html saved"
 
 
