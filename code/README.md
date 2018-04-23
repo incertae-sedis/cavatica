ded1434c6e64df099fb84e2cd58cc145400349ea
j23414 <jenchang@iastate.edu>
2018-04-23 11:21:23 -0500

    Refactor code

---
+++ code/README.md
@@ -0,0 +1,13 @@
+# Code Lookup
+
+Given a list of PubMed ids saved as a text file (e.g. query_pm.ids), fetch the PubMed xml entries.
+
+```
+$ perl pubmed_xml.sh query_pm.ids > query_pm.xml
+```
+
+Given a list of PMC ids saved as a text file (e.g. query_pmc.ids), fetch the PMC xml entries.
+
+```
+$ perl pmc_xml.sh query_pmc.ids > query_pmc.xml
+```
---
+++ code/pmc_xml.sh
@@ -0,0 +1,37 @@
+#! /usr/bin/env bash
+# Auth: Jennifer Chang
+# Date: 2018/03/24
+# Desc: Fetch PubMed xml entries, 100 at a time
+
+# ======================= Variables
+NUM=0;
+QUERY="";
+FILE=100;
+
+# ======================= Main
+[[ -d .pmtmp ]] && rm -rf .pmtmp
+mkdir .pmtmp
+
+while read IDS; do
+    if [ $NUM -ge 100 ]
+    then
+	echo "===== Fetching ids $((FILE-99)) to $FILE" >&2
+	curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc${QUERY}&retmode=xml&tool=ebot" > .pmtmp/${FILE}.xml
+	sleep 0.2;
+	NUM=1;
+	QUERY="&id=${IDS}";
+	FILE=$((FILE+100));
+    else
+	NUM=$((NUM+1))
+	QUERY="$QUERY&id=${IDS}"
+    fi
+done < $1
+
+# ======================= if number of IDs is not a multiple of 100, fetch last group
+if [ $NUM -gt 0 ]
+then
+    echo "===== Fetching ids $((FILE-99)) to $((FILE-100+NUM))" >&2
+    curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pmc${QUERY}&retmode=xml&tool=ebot" > .pmtmp/${FILE}.xml
+fi
+# ======================= printout all the fetched pubmed XML
+cat .pmtmp/*.xml 
