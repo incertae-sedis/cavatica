ba237539082c498bd7df3d342af63dd8ddc3522d
j23414 <jenchang@iastate.edu>
2018-10-09 14:29:20 -0500

    Date Range
    * By default, do not use date range
    * User can modify pmc_ids.sh and pubmed_ids.sh to add in date range

--- code/pubmed_ids.sh
+++ code/pubmed_ids.sh
@@ -4,12 +4,13 @@
 
 # ====== Variables
 TERM=$1
-START=1998/01/01
-ENDIN=2018/01/01
+#START=1998/01/01
+#ENDIN=2018/01/01
 RETMAX=10000
 
 # ===== Analysis
-curl "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\"${TERM}\"&retmax=${RETMAX}&usehistory=y&mindate=${START}&maxdate=${ENDIN}&datetype=PDAT" | sed 's/<RetMax>/|<RetMax>/g' |sed 's/<RetStart>/|<RetStart>/g'|tr '|' '\n' > temp.pm
+#curl -g "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=\"${TERM}\"&retmax=${RETMAX}&usehistory=y&mindate=${START}&maxdate=${ENDIN}&datetype=PDAT" | sed 's/<RetMax>/|<RetMax>/g' |sed 's/<RetStart>/|<RetStart>/g'|tr '|' '\n' > temp.pm
+curl -g "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=${TERM}&retmax=${RETMAX}&usehistory=y&sort=pub+date" | sed 's/<RetMax>/|<RetMax>/g' |sed 's/<RetStart>/|<RetStart>/g'|tr '|' '\n' > temp.pm
 grep "^<Count" temp.pm  >&2
 grep "^<RetMax" temp.pm >&2
 cat temp.pm |grep "<Id>"| sed 's/<Id>//g' |sed 's/<\/Id>//g'
